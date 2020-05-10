39b76c1031e207a68d585996ea2b0439431d3f8f
Dan <dan@instance.id>
2020-05-10 12:35:46 -0500

    dumping files

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "gclone_flutter";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
